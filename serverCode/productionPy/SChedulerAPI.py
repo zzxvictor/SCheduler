cc4ba6ee9ddb0ca05bf0bcf9a7134c9524b18177
zixuan zhang <zhangzix@usc.edu>
2019-09-28 00:14:46 +0000

    update server code - display html

--- serverCode/productionPy/SChedulerAPI.py
+++ serverCode/productionPy/SChedulerAPI.py
@@ -1,13 +1,16 @@
 from flask import Flask, request,jsonify
 from library import scheduler
 #from waitress import serve
+scorer = scheduler.fixedScoreGenerator('.')
+engine = scheduler.greedySearch()
 app = Flask(__name__)
 
 
 @app.route('/hello')
 def hello():
-    print ('Hello')
-    return 'Hello'
+    return '''<h>Welcome USC SCheduler Tool!!!</h>
+              <p>Optimize your academic schedule with artificial intelligence</p>
+            '''
 @app.route('/', methods=['POST'])
 def post():
 	if request.method =='POST':
@@ -16,13 +19,13 @@ def post():
 	try:
 		scheduleList = engine.get_schedules(courseList,scorer,constraint)
 		return jsonify({'schedules':scheduleList, 'success':1})
-	except:
-		return jsonify({'schedules':[], 'success':0})
+	except Exception as e:
+            return jsonify({'schedules':[], 'success':0, 'error': str(e)})
 
 if __name__ == '__main__':
-    scorer = scheduler.fixedScoreGenerator('.')
-    engine = scheduler.greedySearch()
-    app.run(host='0.0.0.0',port = 80, ssl_context='adhoc')
+    #scorer = scheduler.fixedScoreGenerator('.')
+    #engine = scheduler.greedySearch()
+    app.run(host='0.0.0.0' )
     #context.load_cert_chain("server.crt", "server.key")
     #app.run (debug = True, ssl_context='adhoc')
     
